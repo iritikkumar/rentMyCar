9cc1861cf8638b898c28451cb57363e7a8f316dc
Ritik Kumar <thakurritik558@gmail.com>
2023-03-06 13:53:59 +0530

    Redux Store is setup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const { cars } = useSelector(state => state.carsReducer)
   return (
     <DefaultLayout>
         <div>Home Page</div>
+        <p>the length of cars array is {cars.length}</p>
     </DefaultLayout>
   )
 }
---
+++ client/src/redux/reducers/carsReducer.js
@@ -0,0 +1,9 @@
+const initialData = {
+    cars: []
+};
+
+export const carsReducer  = (state = initialData, action)=>{
+    switch(action.type){
+        default: return state
+    }
+}
