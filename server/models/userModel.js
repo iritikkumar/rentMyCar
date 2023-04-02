b0de41ea667c174e59b7e226fd076c4bdc70b42c
Harshita Aggarwal <hashitaaggaral1000@gmail.com>
2023-04-02 11:21:22 +0530

    Driver page

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require("express");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -6,8 +7,10 @@ const userSchema = new mongoose.Schema({
      password : {type:String },
      isAdmin: { type: Boolean, default: false },
      fromGoogle: {type: Boolean, default: false},
-     contactNumber: {type: Number },
+     contactNumber: {type: Number ,  default: 1234567890},
+     location: {type:String, default: "Lucknow"},
      isDriver: {type: Boolean, default: false},
+     image: {type: String, default: "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"},
      },
      { timestamps: true}
 );
